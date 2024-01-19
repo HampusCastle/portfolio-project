acba5bd9e575fbdc152978180afedc087b64ca01
HampusCastle <Hampuscastle@gmail.com>
2024-01-19 15:01:36 +0100

    Second commit

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,108 @@
+"use client";
+import React, { useTransition, useState } from "react";
+import Image from "next/image";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <div className="flex">
+        <div className="pr-8">
+          <h3 className="text-xl font-bold mb-4">Frontend</h3>
+          <ul className="list-disc pl-2">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+          </ul>
+        </div>
+        <div>
+          <h3 className="text-xl font-bold mb-4">Backend</h3>
+          <ul className="list-disc pl-2">
+            <li>Node.js</li>
+            <li>Java</li>
+            <li>MongoDB</li>
+            <li>SQL</li>
+          </ul>
+        </div>
+      </div>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Stockholms Tekniska Institut</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li></li>
+        <li></li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+
+  return (
+    <section className="text-white" id="about">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image src="/images/about-image.png" width={500} height={500} />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+          Hey there! Thanks a bunch for swinging by my page. Super stoked to have you here!
+          If you've got any burning questions, feel free to shoot them my way.
+           Or, you know, just drop a casual "Hello!" - totally cool.
+           Can't wait to chat and I'll hit you back as soon as possible!
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
